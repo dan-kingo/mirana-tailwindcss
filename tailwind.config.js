ffbf4fdc62e07f67009a4b1b04992a8c5475038a
Dan Kingo <danieldejen23@gmail.com>
2024-06-02 12:49:18 +0300

    finish hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-pattern": "url('../images/360.jpg')",
+      },
+    },
   },
   plugins: [],
 };
