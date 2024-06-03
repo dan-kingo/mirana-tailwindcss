89608f9aa3c188fca520b5157b6c0b70483e2f1a
Dan Kingo <danieldejen23@gmail.com>
2024-06-03 09:32:18 +0300

    fix the deploy error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     extend: {
       backgroundImage: {
